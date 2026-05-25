c9791bd6b6842b0a5c218d883d1839660f841be0
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-05-25 22:09:49 +0300

    Cycle through different backgrounds when left/right clicking on the displayed stitched image

--- src/app/stitchedimageview.cpp
+++ src/app/stitchedimageview.cpp
@@ -2,6 +2,8 @@
 
 
 StitchedImageView::StitchedImageView(QWidget *parent) : QGraphicsView(parent) {
+    this->setupBrushes();
+
     this->image_scene = new StitchedImageScene(this);
     this->setScene(this->image_scene);
 }
@@ -19,6 +21,45 @@ void StitchedImageView::clearScene() {
     this->image_scene->clearScene();
 }
 
+void StitchedImageView::setupBrushes() {
+    // Current brush index should start at 0 (Default one)
+    this->current_brush_index = 0;
+
+    // Default brush
+    this->brushes_list.append(Qt::NoBrush);
+
+    // Checkered brush
+    QPixmap tile_pixmap = QPixmap(StitchedImageView::TileSize * 2, StitchedImageView::TileSize * 2);
+    tile_pixmap.fill(Qt::darkGray);
+
+    QPainter tile_painter = QPainter(&tile_pixmap);
+    tile_painter.fillRect(0, 0, StitchedImageView::TileSize, StitchedImageView::TileSize, Qt::gray);
+    tile_painter.fillRect(StitchedImageView::TileSize, StitchedImageView::TileSize, StitchedImageView::TileSize, StitchedImageView::TileSize, Qt::gray);
+    this->brushes_list.append(tile_pixmap);
+
+    // Plain color magenta
+    this->brushes_list.append(Qt::magenta);
+}
+
+void StitchedImageView::updateBackgroundBrush(BrushDirection brush_direction) {
+    switch (brush_direction) {
+        case NEXT:
+        {
+            this->current_brush_index = (this->current_brush_index + 1) % this->brushes_list.size();
+            this->setBackgroundBrush(this->brushes_list[this->current_brush_index]);
+            break;
+        }
+        case PREV:
+        {
+            this->current_brush_index = (this->current_brush_index - 1 + this->brushes_list.size()) % this->brushes_list.size();
+            this->setBackgroundBrush(this->brushes_list[this->current_brush_index]);
+            break;
+        }
+        default:
+          break;
+    }
+}
+
 void StitchedImageView::fitImageScene() {
     QRectF padded_bounding_rect = this->image_scene->getBoundingRect();
     padded_bounding_rect.adjust(
@@ -32,5 +73,23 @@ void StitchedImageView::fitImageScene() {
 void StitchedImageView::resizeEvent(QResizeEvent *event) {
     this->fitImageScene();
     QGraphicsView::resizeEvent(event);
+}
+
+void StitchedImageView::mousePressEvent(QMouseEvent *event) {
+    switch (event->button()) {
+        case Qt::LeftButton:
+        {
+            this->updateBackgroundBrush(BrushDirection::NEXT);
+            break;
+        }
+        case Qt::RightButton:
+        {
+            this->updateBackgroundBrush(BrushDirection::PREV);
+            break;
+        }
+        default:
+            break;
+    }
+    QGraphicsView::mousePressEvent(event);
 }
 
--- src/app/stitchedimageview.hpp
+++ src/app/stitchedimageview.hpp
@@ -5,6 +5,7 @@
 #include <QWidget>
 #include <QGraphicsView>
 #include <QResizeEvent>
+#include <QMouseEvent>
 
 
 class StitchedImageView : public QGraphicsView {
@@ -19,14 +20,34 @@ public:
     void clearScene();
 
 private:
-    StitchedImageScene *image_scene;
+    // Enum to set next/prev background brush
+    enum BrushDirection { NEXT, PREV };
     // ScenePadding to use when fitting the scene
     const int ScenePadding = 5;
+    // Checkered background tile size
+    const int TileSize = 16;
+
+    // Graphics scene
+    StitchedImageScene *image_scene;
+
+    // List of brushes to paint the background
+    // and the index of the current selected brush
+    int current_brush_index;
+    QList<QBrush> brushes_list;
+    // Setup different brushes:
+    // - NoBrush
+    // - Checkered
+    // - Magenta
+    void setupBrushes();
+    // Update background with next/prev brush
+    void updateBackgroundBrush(BrushDirection brush_direction);
+
     // Fit image_scene in view keeping aspect ratio and adding padding on all sides
     void fitImageScene();
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
 
 };
 
